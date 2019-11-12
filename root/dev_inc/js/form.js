a657965237175ab580a5aa390d50af6af6509bfc
박홍식B님/서비스기획 <wlphs@naver.com>
2019-11-12 14:32:31 +0900

    merge

--- root/dev_inc/js/form.js
+++ root/dev_inc/js/form.js
@@ -91,6 +91,51 @@ jQuery.fn.allCheckbox = function( $check_item){
     })
 }
 
+
+jQuery.fn.oneCheckbox = function( $check_item_option){
+    var _one = $(this);
+    _one.on('change', function() {
+        $($check_item_option).prop('checked', $(this).prop('checked'))
+    });
+    $($check_item_option).on('change', function () {
+        _one.prop('checked', $($check_item_option+':checked').length);
+    })
+}
+
+
+jQuery.fn.allCheckbox_option = function( $check_item, $options_boss,$check_item_option){
+    var _all = $(this);
+    var len = $($check_item).length;
+    _all.on('change', function() {
+        $($check_item).prop('checked', $(this).prop('checked'))
+        $($check_item_option).prop('checked', $(this).prop('checked'))
+    });
+    $($check_item).on('change', function () {
+        _all.prop('checked', $($check_item+':checked').length == len);
+    })
+
+    var _one = $($options_boss);
+    _one.on('change', function() {
+        $($check_item_option).prop('checked', $(this).prop('checked'))
+    });
+    $($check_item_option).on('change', function () {
+        console.log($($check_item_option+':checked').length)
+        _one.prop('checked', $($check_item_option+':checked').length);
+        _all.prop('checked', $($check_item+':checked').length == len);
+    })
+}
+
+jQuery.fn.oneCheckbox_all = function( $check_item_option){
+    var _one = $(this);
+    _one.on('change', function() {
+        $($check_item_option).prop('checked', $(this).prop('checked'))
+    });
+    $($check_item_option).on('change', function () {
+        _one.prop('checked', $($check_item_option+':checked').length);
+    })
+}
+
+
 var datepicker_yy_mm_dd = {
     dateFormat: "yy-mm-dd", 
     showMonthAfterYear:true,
